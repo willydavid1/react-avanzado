f4c1538b169ba612c5d407072347e58adfbafdcb
Willy Da Conceicao <willydavidlozada@gmail.com>
2020-08-08 12:02:23 -0500

    Fetch data API

--- src/components/ListOfCategories/index.js
+++ src/components/ListOfCategories/index.js
@@ -1,10 +1,16 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { Category } from '../Category'
 
 import { List, Item } from './styles'
-import { categories } from '../../../api/db.json'
 
 export const ListOfCategories = () => {
+  const [categories, setCategories] = useState([])
+  useEffect(() => {
+    // eslint-disable-next-line no-undef
+    fetch('https://petgram-server-willydavid.vercel.app/categories')
+      .then(res => res.json())
+      .then(res => setCategories(res))
+  }, [])
   return (
     <List>
       {
