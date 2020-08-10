2de081ffc3ba20c8be904b6ef3f76f3220195f7e
Willy Da Conceicao <willydavidlozada@gmail.com>
2020-08-09 21:09:50 -0500

    ¿Qué es GraphQL y React Apollo? Inicializando React Apollo Client y primer HoC

--- src/components/ListOfPhotoCards/index.js
+++ src/components/ListOfPhotoCards/index.js
@@ -1,10 +1,28 @@
 import React from 'react'
 import { PhotoCard } from '../PhotoCard'
+import { graphql } from 'react-apollo'
+import { gql } from 'apollo-boost'
 
-export const ListOfPhotoCards = () => {
+const withPhotos = graphql(gql`
+  query getPhotos {
+    photos {
+      id,
+      categoryId,
+      src,
+      likes,
+      userId,
+      liked
+    }
+  }
+`)
+
+const ListOfPhotoCardsComponent = ({ data: { photos = [] } } = {}) => {
   return (
     <ul>
-      {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map(id => <PhotoCard key={id} id={id} />)}
+      {photos.map((photo) => <PhotoCard key={photo.id} {...photo} />)}
     </ul>
   )
 }
+
+// HoC para el componente ListOfPhotoCardsComponent con la query de withPhotos
+export const ListOfPhotoCards = withPhotos(ListOfPhotoCardsComponent)
