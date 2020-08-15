916db6951ada57a12432d814488108eddde8bbaf
Willy Da Conceicao <willydavidlozada@gmail.com>
2020-08-15 11:26:19 -0500

    Rutas protegidas

--- src/App.js
+++ src/App.js
@@ -2,11 +2,19 @@
 import React from 'react'
 import { GlobalStyle } from './styles/GlobalStyles'
 import { Logo } from './components/Logo'
-import { Home } from './pages/Home'
-import { Detail } from './pages/Detail'
 import { NavBar } from './components/NavBar'
 
+import { Home } from './pages/Home'
+import { Detail } from './pages/Detail'
+import { Favs } from './pages/Favs'
+import { User } from './pages/User'
+import { NotRegisteredUser } from './pages/NotRegisteredUser'
+
 import { Router } from '@reach/router'
+
+const UserLogged = ({ children }) => {
+  return children({ isAuth: false })
+}
 
 export const App = () => {
   return (
@@ -18,6 +26,23 @@ export const App = () => {
         <Home path='/pet/:categoryId' />
         <Detail path='/detail/:detailId' />
       </Router>
+
+      {/* Rutas protegidas */}
+      <UserLogged>
+        {
+          ({ isAuth }) => (
+            isAuth
+              ? (<Router>
+                <Favs path='/favs' />
+                <User path='/user' />
+              </Router>)
+              : (<Router>
+                <NotRegisteredUser path='/favs' />
+                <NotRegisteredUser path='/user' />
+              </Router>)
+          )
+        }
+      </UserLogged>
       <NavBar />
     </>
   )
