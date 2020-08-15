6fa93f6c5bdb8b58c32c436bee3e64567e3e23fc
Willy Da Conceicao <willydavidlozada@gmail.com>
2020-08-15 10:20:25 -0500

    Pagina Detail

--- src/App.js
+++ src/App.js
@@ -1,31 +1,22 @@
 /* eslint-disable react/jsx-closing-tag-location */
 import React from 'react'
 import { GlobalStyle } from './styles/GlobalStyles'
-import { PhotoCardWithQuery } from './container/PhotoCardWithQuery'
 import { Logo } from './components/Logo'
 import { Home } from './pages/Home'
+import { Detail } from './pages/Detail'
 
 import { Router } from '@reach/router'
 
 export const App = () => {
-  const urlParams = new window.URLSearchParams(window.location.search)
-  const detailId = urlParams.get('detail')
-
   return (
     <>
       <GlobalStyle />
       <Logo />
-      {
-        detailId
-          ? <PhotoCardWithQuery id={detailId} />
-          : (
-            <Router>
-              <Home path='/' />
-              <Home path='/pet/:categoryId' />
-            </Router>
-          )
-      }
-
+      <Router>
+        <Home path='/' />
+        <Home path='/pet/:categoryId' />
+        <Detail path='/detail/:detailId' />
+      </Router>
     </>
   )
 }
