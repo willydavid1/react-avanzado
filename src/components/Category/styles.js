4d4c190cd9221baf0efbb25aeb574440cb6f4145
Willy Da Conceicao <willydavidlozada@gmail.com>
2020-08-15 10:07:00 -0500

    Link | (reach router)

--- src/components/Category/styles.js
+++ src/components/Category/styles.js
@@ -1,6 +1,8 @@
 import styled from 'styled-components'
+import { Link as LinkRouter } from '@reach/router'
 
-export const Anchor = styled.a`
+// styled recibe un componente pero tiene que aceptar la prop className
+export const Link = styled(LinkRouter)` 
     display: flex;
     flex-direction: column;
     text-align: center;
