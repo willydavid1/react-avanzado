241a18a813400bcc9d2eae53b102be3e04aca5cb
Willy Da Conceicao <willydavidlozada@gmail.com>
2020-08-17 16:41:45 -0500

    Ultimos retoques a las rutas

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 /* eslint-disable react/jsx-closing-tag-location */
-import React from 'react'
+import React, { useContext } from 'react'
 import { GlobalStyle } from './styles/GlobalStyles'
 import { Logo } from './components/Logo'
 import { NavBar } from './components/NavBar'
@@ -9,37 +9,33 @@ import { Detail } from './pages/Detail'
 import { Favs } from './pages/Favs'
 import { User } from './pages/User'
 import { NotRegisteredUser } from './pages/NotRegisteredUser'
+import { NotFound } from './pages/NotFound'
 
-import { Router } from '@reach/router'
-import Context from './Context'
+import { Router, Redirect } from '@reach/router'
+import { Context } from './Context'
 
 export const App = () => {
+  const { isAuth } = useContext(Context)
   return (
     <>
       <GlobalStyle />
       <Logo />
       <Router>
+        <NotFound default />
+
         <Home path='/' />
         <Home path='/pet/:categoryId' />
         <Detail path='/detail/:detailId' />
+        {!isAuth && <NotRegisteredUser path='/login' />}
+        {!isAuth && <Redirect noThrow from='/favs' to='/login' />}
+        {!isAuth && <Redirect noThrow from='/user' to='/login' />}
+        {isAuth && <Redirect noThrow from='/login' to='/' />}
+
+        {/* Rutas protegidas */}
+        <Favs path='/favs' />
+        <User path='/user' />
       </Router>
 
-      {/* Rutas protegidas */}
-      <Context.Consumer>
-        {
-          ({ isAuth }) => (
-            isAuth
-              ? (<Router>
-                <Favs path='/favs' />
-                <User path='/user' />
-              </Router>)
-              : (<Router>
-                <NotRegisteredUser path='/favs' />
-                <NotRegisteredUser path='/user' />
-              </Router>)
-          )
-        }
-      </Context.Consumer>
       <NavBar />
     </>
   )
---
+++ src/pages/NotFound.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const NotFound = () => {
+  return (
+    <div>
+      <h2> Esta página no existe! </h2>
+    </div>
+  )
+}
