7f2be77d02120ff6ab5dfc5843b576be4a749319
Willy Da Conceicao <willydavidlozada@gmail.com>
2020-08-17 17:56:28 -0500

    React Helmet

--- src/pages/Favs.js
+++ src/pages/Favs.js
@@ -1,9 +1,9 @@
 import React from 'react'
 import { FavsWithQuery } from '../container/GetFavorites'
+import { Layout } from '../components/Layout'
 
 export const Favs = () => (
-  <>
-    <h2>Favs - photos LIKED</h2>
+  <Layout title='Tus Favoritos' subtitle='Aqui puedes encontrar tus favoritos.'>
     <FavsWithQuery />
-  </>
+  </Layout>
 )
