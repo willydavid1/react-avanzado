5a148f1e65aac29cf68cfd14ddfb3215aecd8f93
Willy Da Conceicao <willydavidlozada@gmail.com>
2020-08-17 12:00:00 -0500

    Hacer like como usuario registrado

--- src/components/PhotoCard/index.js
+++ src/components/PhotoCard/index.js
@@ -2,7 +2,6 @@
 import React from 'react'
 import { Article, ImgWrapper, Img } from './styles'
 
-import { useLocalStorage } from '../../hooks/useLocalStorage'
 import { useNearScreen } from '../../hooks/useNearScreen'
 
 import { FavButton } from '../FavButton'
@@ -11,9 +10,7 @@ import { Link } from '@reach/router'
 
 const DEFAULT_IMAGE = 'https://res.cloudinary.com/midudev/image/upload/w_300/q_80/v1560262103/dogs.png'
 
-export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMAGE }) => {
-  const key = `like-${id}`
-  const [liked, setLiked] = useLocalStorage(key, false) // CUSTOM HOOK
+export const PhotoCard = ({ id, liked, likes = 0, src = DEFAULT_IMAGE }) => {
   const [show, element] = useNearScreen() // CUSTOM HOOK
 
   return (
@@ -30,12 +27,11 @@ export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMAGE }) => {
             {
               (toggleLike) => {
                 const handleFavClick = () => {
-                  !liked && toggleLike({
+                  toggleLike({
                     variables: {
                       input: { id }
                     }
                   })
-                  setLiked(!liked)
                 }
 
                 return <FavButton liked={liked} likes={likes} onClick={handleFavClick} />
