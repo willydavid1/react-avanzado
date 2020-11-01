1367e2edac1e6f21787cf07b9e68ffe2a0da6516
Willy Da Conceicao <willydavidlozada@gmail.com>
2020-10-31 21:41:02 -0500

    React.lazy() y componente Suspense

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 /* eslint-disable react/jsx-closing-tag-location */
-import React, { useContext } from 'react'
+import React, { useContext, Suspense } from 'react'
 import { GlobalStyle } from './styles/GlobalStyles'
 import { Logo } from './components/Logo'
 import { NavBar } from './components/NavBar'
 
 import { Home } from './pages/Home'
 import { Detail } from './pages/Detail'
-import { Favs } from './pages/Favs'
+// import { Favs } from './pages/Favs'
 import { User } from './pages/User'
 import { NotRegisteredUser } from './pages/NotRegisteredUser'
 import { NotFound } from './pages/NotFound'
@@ -14,10 +14,12 @@ import { NotFound } from './pages/NotFound'
 import { Router, Redirect } from '@reach/router'
 import { Context } from './Context'
 
+const Favs = React.lazy(() => import('./pages/Favs'))
+
 export const App = () => {
   const { isAuth } = useContext(Context)
   return (
-    <>
+    <Suspense fallback={<div />}>
       <GlobalStyle />
       <Logo />
       <Router>
@@ -37,6 +39,6 @@ export const App = () => {
       </Router>
 
       <NavBar />
-    </>
+    </Suspense>
   )
 }
